7ae83e456e647b85dcd30eacb4ab7b81a037c88c
Bekzhan <114474251+rakh1mbayev@users.noreply.github.com>
2025-02-22 12:18:17 +0600

    Update authController.js

--- authController.js
+++ authController.js
@@ -10,7 +10,7 @@ const generateAccessToken = (id, roles) => {
         id,
         roles
     }
-    return jwt.sign(payload, secret, {expiresIn: "24h"});
+    return jwt.sign(payload, secret, {expiresIn: "6h"});
 }
 
 class authContoller {
